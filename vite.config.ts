8250abe86f46273f175e30a3bc3025cf0108dc4d
Christopher Tonnesland <christopher.tonnesland@redgo.no>
2024-09-15 10:02:49 +0200

    changing link to admin

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'index.html', // The file to copy
-          dest: '.',          // Destination directory (dist folder)
-          rename: '404.html'  // Name it 404.html
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
    base: '/'
 })
